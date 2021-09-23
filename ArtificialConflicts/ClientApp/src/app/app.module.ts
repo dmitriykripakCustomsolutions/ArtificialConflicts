f245e72c6eeba002b2ddde1ba5865145e97ee3a7
RuslanKudlay <86481893+RuslanKudlay@users.noreply.github.com>
2021-09-23 16:39:05 +0300

    fix errors

--- ArtificialConflicts/ClientApp/src/app/app.module.ts
+++ ArtificialConflicts/ClientApp/src/app/app.module.ts
@@ -4,15 +4,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
   import { HttpClientModule } from '@angular/common/http';
   import { RouterModule } from '@angular/router';
-
-    
     import { NavMenuComponent } from './nav-menu/nav-menu.component';
     import { HomeComponent } from './home/home.component';
     import { CounterComponent } from './counter/counter.component';
     import { FetchDataComponent } from './fetch-data/fetch-data.component';
   import { AppComponent } from './app.component';
-  import { NavMenuComponent } from './nav-menu/nav-menu.component';
-  import { HomeComponent } from './home/home.component';
   
   
 
